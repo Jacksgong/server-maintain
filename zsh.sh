3c4fd9c0b007045e0e08cc42e2319d04d06df36b
Jacksgong <igzhenjie@gmail.com>
2017-03-04 23:07:50 +0800

    feat: add the script for init oh-my-zsh environment

---
+++ zsh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This file is licensed under the Apache License, Version 2.0
+# Copyright (C) 2017 Jacksgong(blog.dreamtobe.cn)
+
+# This script used for restore oh-my-zsh environment
+
+apt-get update
+apt-get install zsh
+apt-get install git-core
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+cp .source/.zshrc ~/.zshrc
+
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+chsh -s `which zsh`
+shutdown -r 0
